55711bcd26789a9817d0a661db727a72ddc98574
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-20 00:57:51 +0000

    Deploy rogeroliveira84/react-redux-crud to github.com/rogeroliveira84/react-redux-crud.git:gh-pages

---
+++ precache-manifest.3eb6b30357c01461d18531bea9b987e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5925666f492c119ff3c26440a2188450",
+    "url": "/rogeroliveira84/react-redux-crud/index.html"
+  },
+  {
+    "revision": "e4e8ad4390bf32768a2a",
+    "url": "/rogeroliveira84/react-redux-crud/static/css/main.39285c12.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/rogeroliveira84/react-redux-crud/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "e4e8ad4390bf32768a2a",
+    "url": "/rogeroliveira84/react-redux-crud/static/js/main.d25e2820.chunk.js"
+  },
+  {
+    "revision": "6bc33117ce3f329a06ca",
+    "url": "/rogeroliveira84/react-redux-crud/static/js/runtime~main.b7015d5a.js"
+  }
+]);
